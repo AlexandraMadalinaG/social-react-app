eb903697a7887594cb94c5eeb4edff21f57cf806
AlexandraMadalinaG <madalinagrigore62@yahoo.com>
2023-10-16 18:32:27 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
             <div className="container">
                 <Routes>
-                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/home" element={<Home />}></Route>
                     <Route path="/login" element={<LogIn />}></Route>
                 </Routes>
             </div>

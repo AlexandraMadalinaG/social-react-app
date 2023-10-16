6ba915dfe9e7af9a5d9e2a914de32c3fb6629878
AlexandraMadalinaG <madalinagrigore62@yahoo.com>
2023-10-16 18:24:31 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import LogIn from './pages/login/LogIn';
 import TopBar from './components/topbar/TopBar';
